645f292391c28138fcefe3a1b2c5ace6119b0aca
Ivan <ivan.yarmoliuk@gmail.com>
2023-04-27 11:15:19 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ivan Yarmoliuk on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
